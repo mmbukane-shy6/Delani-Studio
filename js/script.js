ccbcec7f8f007e6375e653ee17c62bf1a7c0ed4f
mmbukaneshy6 <sheilaegeidza@gmail.com>
2020-02-22 21:38:28 +0300

    added toggle

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
 $(document).ready(function(){
-    //Design toggle
+    //toggle
     $(".design").click(function(){
+        $(".shy design-shy").show();
+        $(this).hide();
+    })
+    $(".design-shy").click(function(){
         $(".design").show();
         $(this).hide();
     })
-    $(".design-par").click(function(){
-        $(".design").show();
+    $(".development").click(function(){
+        $(".shy development-shy").show();
+        $(this).hide();
+    })
+    $(".development-shy").click(function(){
+        $(".development").show();
+        $(this).hide();
+    })
+    $(".product").click(function(){
+        $(".shy product-shy").show();
+        $(this).hide();
+    })
+    $(".product-shy").click(function(){
+        $(".product").show();
         $(this).hide();
     })
 
