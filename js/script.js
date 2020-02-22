11376d32bfe055543a6efd1c9b8558e95b412619
mmbukaneshy6 <sheilaegeidza@gmail.com>
2020-02-22 20:19:03 +0300

    added js link

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    //Design toggle
+    $(".design").click(function(){
+        $(".design").show();
+        $(this).hide();
+    })
+    $(".design-par").click(function(){
+        $(".design").show();
+        $(this).hide();
+    })
+
+})
