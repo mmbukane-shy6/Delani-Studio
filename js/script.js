54676c2e20fc68e37e4e44e229005ca3c76d8d75
mmbukaneshy6 <sheilaegeidza@gmail.com>
2020-02-24 09:38:58 +0300

    changed js features

--- js/script.js
+++ js/script.js
@@ -1,91 +1,76 @@
-$(document).ready(function(){
-    //toggle
+$(document).ready(function () {
+//toggle
     $(".design").click(function(){
-        $(".shy design-shy").show();
-        $(this).hide();
-    })
-    $(".design-shy").click(function(){
-        $(".design").show();
-        $(this).hide();
-    })
-    $(".development").click(function(){
-        $(".shy development-shy").show();
-        $(this).hide();
-    })
-    $(".development-shy").click(function(){
-        $(".development").show();
-        $(this).hide();
-    })
+        $("#design").toggle();
+    });
+    $(".dev").click(function(){
+        $("#dev").toggle();
+    });
     $(".product").click(function(){
-        $(".shy product-shy").show();
-        $(this).hide();
-    })
-    $(".product-shy").click(function(){
-        $(".product").show();
-        $(this).hide();
-    })
+        $("#product").toggle();
+    });
     //portfolio
-    $(".pic").mouseenter(function(){
+    $(".pic").mouseenter(function () {
         $(".portfolio-1").show();
     })
-    $(".pic").mouseleave(function(){
+    $(".pic").mouseleave(function () {
         $(".portfolio-1").hide();
     })
-    $(".pic").mouseenter(function(){
+    $(".pic").mouseenter(function () {
         $(".portfolio-2").show();
     })
-    $(".pic").mouseleave(function(){
+    $(".pic").mouseleave(function () {
         $(".portfolio-2").hide();
     })
-    $(".pic").mouseenter(function(){
+    $(".pic").mouseenter(function () {
         $(".portfolio-3").show();
     })
-    $(".pic").mouseleave(function(){
+    $(".pic").mouseleave(function () {
         $(".portfolio-3").hide();
     })
-    $(".pic").mouseenter(function(){
+    $(".pic").mouseenter(function () {
         $(".portfolio-4").show();
     })
-    $(".pic").mouseleave(function(){
+    $(".pic").mouseleave(function () {
         $(".portfolio-4").hide();
     })
-    $(".pic").mouseenter(function(){
+    $(".pic").mouseenter(function () {
         $(".portfolio-5").show();
     })
-    $(".pic").mouseleave(function(){
+    $(".pic").mouseleave(function () {
         $(".portfolio-5").hide();
     })
-    $(".pic").mouseenter(function(){
+    $(".pic").mouseenter(function () {
         $(".portfolio-6").show();
     })
-    $(".pic").mouseleave(function(){
+    $(".pic").mouseleave(function () {
         $(".portfolio-6").hide();
     })
-    $(".pic").mouseenter(function(){
+    $(".pic").mouseenter(function () {
         $(".portfolio-7").show();
     })
-    $(".pic").mouseleave(function(){
+    $(".pic").mouseleave(function () {
         $(".portfolio-7").hide();
     })
-    $(".pic").mouseenter(function(){
+    $(".pic").mouseenter(function () {
         $(".portfolio-8").show();
     })
-    $(".pic").mouseleave(function(){
+    $(".pic").mouseleave(function () {
         $(".portfolio-8").hide();
     })
     //submit button
-    $("#submit").click(function(event){
+    $("#submit").click(function (event) {
         event.preventDefault();
-        var name=$("#name").val();
-        var mail=$("mail").val();
-        var text=$("#text").val();
+        var name = $("#name").val();
+        var mail = $("mail").val();
+        var text = $("#text").val();
         var key = '43ab10333d46e19325f96d210ed40c49-us5';
-        if(name=="" || mail=="" || text==""){
-            alert("hi" + " " +name + " " +"we have recieved Your message .Thank you for reaching out to us")
+        if (name == "" || mail == "" || text == "") {
+            alert("hi" + " " + name + " " + "we have recieved Your message .Thank you for reaching out to us")
 
-        }else{
+        } else {
             alert("please make sure you have filled in the form correctly")
- 
+
         }
 
     })
