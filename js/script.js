07cc5ff47199242d933fd589e1c61e026f4e4dfa
mmbukaneshy6 <sheilaegeidza@gmail.com>
2020-02-24 12:56:26 +0300

    modified css

--- js/script.js
+++ js/script.js
@@ -64,7 +64,7 @@ $(document).ready(function () {
         var name = $("#name").val();
         var mail = $("mail").val();
         var text = $("#text").val();
-        var key = '43ab10333d46e19325f96d210ed40c49-us5';
+        //var key = '43ab10333d46e19325f96d210ed40c49-us5';//
         if (name == "" || mail == "" || text == "") {
             alert("hi" + " " + name + " " + "we have recieved Your message .Thank you for reaching out to us")
 
